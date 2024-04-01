c82375b097a63c18e2712b344bb043b112c3dc9f
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2024-04-01 06:43:43 -0500

    feat: :sparkles: Add prng handler to generate pseudo-random numbers

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,32 @@
 import yargs from "yargs";
+import prng from "./prng";
 
 const { argv } = yargs
   .options({})
   .command({
     command: 'prng',
     describe: 'Generate a pseudo-random number',
-    handler: () => {},
+    handler: ({ type, size, min, max, encoding }) => {
+      console.log(prng(type, size, min, max, encoding));
+    },
     builder: {
       type: {
-        choices: ["bytes", "int", "uuid"] as const,
+        choices: ['bytes', 'int', 'uuid'] as const,
         description: '',
         demandOption: true
       },
-      sixe: {
+      size: {
         alias: 's',
         description: 'Random number size',
         default: 16
       },
-      max: {
-        type: 'number',
-        default: 0
-      },
       min: {
         type: 'number',
         default: 100
+      },
+      max: {
+        type: 'number',
+        default: 0
       },
       encoding: {
         alias: 'enc',
@@ -42,8 +45,8 @@ const { argv } = yargs
           "hex"
         ] as const,
         default: "hex"
-      }
-    }
+      },
+    },
   })
   .demandCommand(1, "You need at least one command before moving on")
   .help();
